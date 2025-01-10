366ebf6d0de36b9d4537aa24315b7b846d06d11f
miky <mjm.vermeulen@hotmail.com>
2025-01-10 23:12:01 +0100

    Added option for player to request information about the different player stats

--- utils.py
+++ utils.py
@@ -1,6 +1,22 @@
 import os
 
 def clear_terminal():
+    """
+    Clears the terminal from the previous scene to ensure that we keep a clean view and scenes do not overlap
+    """
     print('\n'*80) # terminal clear for development
 
     # os.system('cls' if os.name=='nt' else 'clear')
+
+def player_stat_details():
+    """
+    Provides the player with information about the different stats
+    """
+    print("\033[1mCharacter stat overview:\033[0m\n"
+          "health: Determines how much damage you can take before you are defeated\n"
+          "attack: Determines how much damage you deal\n"
+          "defence: Determines how much damage you take, the higher the number the less damage you take\n"
+          "evasion: Gives you a chance to dodge attacks (this means you take no damage), its a percentage chance\n"
+          "critrate: Gives you a chance to hit for critical dmg (1,5x dmg), its a percentage chance\n")
+
+    input("Take your time to read all the information, once you are ready press any key to continue ......")
